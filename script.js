55cae0b9dfe8b5b8da589d57d75e9b16ed14ff47
Gleydsonnoronham <gleydson.mnoronha@gmail.com>
2025-10-28 17:46:53 -0300

    Nova url Nova webapp

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ try {
 ========================================= */
 async function submitToGoogleSheets(formData) {
   try {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxxWDHfpqt9Tdz6sm7dF4Y0iwFkeAhmLNBlSEgM-X4QMZ9mJ1OjBoh55nAje6wUWlYv/exec';
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz9HLAJl0s3dww2Y_DkgWC9zWzLhnkYyDd7Ha3KaEEm9e1l_lW9PK7V36hlzQEUVKbD/exec';
     
     const params = new URLSearchParams();
     for (let [key, value] of formData.entries()) {
